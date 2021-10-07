4043f14ac6d8bd46c19bf17024260654befde0aa
arioamriihsan <arioamri@gmail.com>
2021-10-07 23:55:50 +0700

    bug fixing when movie not found

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,13 +6,13 @@ import React, {
   useCallback,
 } from "react";
 import { MovieList } from "../components";
-import axios from 'axios';
-import { API_URL } from '../support/api';
+import axios from "axios";
+import { API_URL } from "../support/api";
 import { useMovieContext } from "../components/MovieContext";
 
 const HomePage = () => {
   const [movies, setMovies] = useState([]);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [movieData, setMovieData] = useState([]);
 
   // Context
@@ -27,33 +27,36 @@ const HomePage = () => {
       setPage: movieContext.setPage,
       suggestion: movieContext.suggestion,
       setSuggestion: movieContext.setSuggestion,
-      setIsDetail: movieContext.setIsDetail
+      setIsDetail: movieContext.setIsDetail,
     };
   }, [movieContext]);
 
-  let { 
-    search, 
-    page, 
-    setHasMore, 
-    hasMore, 
-    setPage, 
-    setSearch, 
+  let {
+    search,
+    page,
+    setHasMore,
+    hasMore,
+    setPage,
+    setSearch,
     setSuggestion,
-    setIsDetail 
+    setIsDetail,
   } = movieProps;
 
   const observer = useRef();
-  const lastMovieElementRef = useCallback((node) => {
-    if (observer.current) observer.current.disconnect();
-    observer.current = new IntersectionObserver(entries => {
-      if (entries[0].isIntersecting && hasMore) {
-        setPage(prevPage => prevPage + 1);
+  const lastMovieElementRef = useCallback(
+    (node) => {
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPage((prevPage) => prevPage + 1);
+        }
+      });
+      if (node) {
+        observer.current.observe(node);
       }
-    });
-    if (node) {
-      observer.current.observe(node);
-    }
-  }, [hasMore]);
+    },
+    [hasMore]
+  );
 
   useEffect(() => {
     setIsDetail(false);
@@ -67,14 +70,15 @@ const HomePage = () => {
 
   useEffect(() => {
     if (!search) {
-      setSearch('batman');
+      setSearch("batman");
     }
     setMovies([]);
     setSuggestion([]);
   }, [search]);
-
+  
   useEffect(() => {
     let cancel;
+    setError("");
     axios({
       method: "GET",
       url: API_URL,
@@ -82,17 +86,18 @@ const HomePage = () => {
       cancelToken: new axios.CancelToken((c) => (cancel = c)),
     })
       .then((resp) => {
-        console.log(resp.data);
-        if (resp.data.Search) {
-          setMovies(prevMovies => {
-            return [...prevMovies, ...resp.data.Search.map(el => el)];
+        if (resp.data.Search && !error) {
+          setMovies((prevMovies) => {
+            return [...prevMovies, ...resp.data.Search.map((el) => el)];
           });
-          if (search && search !== 'batman' && page === 1) {
+          if (search && search !== "batman" && page === 1) {
             setSuggestion(resp.data.Search);
           }
           setHasMore(resp.data.Search.length > 0);
         }
         if (resp.data.Error) {
+          setMovieData([]);
+          setMovies([]);
           setError(resp.data.Error);
         }
       })
@@ -104,16 +109,17 @@ const HomePage = () => {
       });
     return () => cancel();
   }, [search, page]);
-  
+
   return (
     <div className="app-container">
       <div className="d-flex flex-row flex-wrap justify-content-center align-items-center">
-        {movieData.length > 0 ? (
+        {movieData.length > 0 && (
           <MovieList
             movies={movieData}
             lastMovieElementRef={lastMovieElementRef}
           />
-        ) : (
+        )}
+        {error && (
           <div style={{ height: "35rem" }}>
             <h3 style={{ color: "#fff" }}>{error}</h3>
           </div>
